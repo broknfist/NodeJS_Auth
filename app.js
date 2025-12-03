14f615a3de925af698cd6d9bd6cb48203ab04b3e
Zsolt Nyemetz <zsolt.nyemetz@hotmail.com>
2025-12-04 00:02:19 +0100

    to linux
    Its faster to develop into production

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const CONTEXT_PATH = process.env.CONTEXT_PATH || '';
 
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,8 +21,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/app148/', indexRouter);
+app.use('/app148/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
